0a744a5f87e7672ea5b48026f879d7c92bd3c50c
ciherraiz <ciherraiz@gmail.com>
2022-07-28 20:39:46 +0200

    mostrar el aula

--- pages/03_inscripción.py
+++ pages/03_inscripción.py
@@ -13,5 +13,5 @@ df_filtrado['rsp'] = df_filtrado['solicitudes'] / df_filtrado['plazas']
 df_filtrado['dias_solicitud'] = (datetime.datetime.now() - df_filtrado['ins_inicio']).dt.days
 df_filtrado['sol_por_dia'] = df_filtrado['solicitudes'] / df_filtrado['dias_solicitud']
 st.write(f'Fecha fin de inscripción mayor o igual a {hoy} ({len(df_filtrado)} cursos)')
-cols = ['titulo', 'rsp', 'dias_fin', 'ins_inicio', 'ins_fin', 'centro', 'provincia', 'plazas', 'solicitudes', 'sol_por_dia', 'momento']
+cols = ['titulo', 'rsp', 'dias_fin', 'ins_inicio', 'ins_fin', 'centro', 'provincia', 'aula', 'plazas', 'solicitudes', 'sol_por_dia', 'momento']
 st.dataframe(df_filtrado[cols].style.format(precision=2).background_gradient(subset=['rsp']))
